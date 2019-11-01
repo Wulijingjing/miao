5aff4b92c0cf363bae416312f99c299252622b90
Wulijingjing <1131069347@qq.com>
2019-11-01 09:38:41 +0800

    add createcomponents

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import axios from 'axios'
+Vue.prototype.axios = axios;
+//vue就是一个类，给这个类添加一个属性，就可以用属性来用axios
 Vue.config.productionTip = false
 
 new Vue({
